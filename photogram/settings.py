dfa38f0912931fc8262af290a1545a7ec99320a9
Alchemy <maanoatu17@gmail.com>
2018-05-15 12:25:26 +0300

    added code for deployment

--- photogram/settings.py
+++ photogram/settings.py
@@ -135,4 +135,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-TATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
